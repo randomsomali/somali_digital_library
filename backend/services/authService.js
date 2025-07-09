79238cf1901f776296ce4252d1dbd78b9c60b6ae
Husein Hashi <huseinarahman6@gmail.com>
2025-07-09 21:29:31 +0300

    last commit

--- backend/services/authService.js
+++ backend/services/authService.js
@@ -1,12 +1,17 @@
-import bcrypt from 'bcryptjs';
-import User from '../models/userModel.js';
-import Admin from '../models/adminModel.js';
-import Institution from '../models/institutionModel.js';
-import RefreshToken from '../models/refreshTokenModel.js';
-import { generateTokens } from '../utils/jwt.js';
+import bcrypt from "bcryptjs";
+import User from "../models/userModel.js";
+import Admin from "../models/adminModel.js";
+import Institution from "../models/institutionModel.js";
+import RefreshToken from "../models/refreshTokenModel.js";
+import { generateTokens } from "../utils/jwt.js";
 
 class AuthService {
-  static async saveRefreshToken(token, userId = null, adminId = null, institutionId = null) {
+  static async saveRefreshToken(
+    token,
+    userId = null,
+    adminId = null,
+    institutionId = null
+  ) {
     return RefreshToken.save(token, userId, adminId, institutionId);
   }
 
@@ -27,25 +32,25 @@ class AuthService {
   }
 
   static setCookies(res, { accessToken, refreshToken }) {
-    res.cookie('accessToken', accessToken, {
+    res.cookie("accessToken", accessToken, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
-      maxAge: 15 * 60 * 1000 // 15 minutes
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "none",
+      maxAge: 15 * 60 * 1000, // 15 minutes
     });
 
-    res.cookie('refreshToken', refreshToken, {
+    res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
-      maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "none",
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
   }
 
   static clearCookies(res) {
-    res.cookie('accessToken', '', { maxAge: 0 });
-    res.cookie('refreshToken', '', { maxAge: 0 });
+    res.cookie("accessToken", "", { maxAge: 0 });
+    res.cookie("refreshToken", "", { maxAge: 0 });
   }
 }
 
-export default AuthService; 
+export default AuthService;
